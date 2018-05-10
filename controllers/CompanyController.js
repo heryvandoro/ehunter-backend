75ff6d3e06b3e57e4b663ce3c61b3e96cb02b4f2
Hery Vandoro <vandorohery99@gmail.com>
2018-05-10 23:46:14 +0700

    Install bcrypt, hash password on company

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const db = require("../db.js");
+const bcrypt = require('bcrypt');
 
 router.get("/", async (req, res) => {
     let companies = await db.Company.findAll({
@@ -18,11 +19,11 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+    let password = await bcrypt.hash(req.body.password, 10);
     let company = await db.Company.create({
         name : req.body.name,
         email : req.body.email,
-        password : req.body.password,
-        description : req.body.description
+        password : password
     });
     res.send(company);
 });
@@ -31,9 +32,10 @@ router.patch("/:id", async (req, res) => {
     let company = await db.Company.findById(req.params.id);
     if(!company) res.send({ messages : "data not found" });
     
+    let password = await bcrypt.hash(req.body.password, 10);
     company.name = req.body.name;
     company.email = req.body.email;
-    company.password = req.body.password;
+    company.password = password;
     company.description = req.body.description;
 
     company = await company.save();
